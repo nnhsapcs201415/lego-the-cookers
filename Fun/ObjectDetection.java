8f61f19e7960674b3547c8f0f93c5327837e355b
ncserpico <NCSerpico@student.naperville203.org>
2017-05-10 13:41:49 -0500

    ugh

--- Fun/ObjectDetection.java
+++ Fun/ObjectDetection.java
@@ -6,26 +6,34 @@ import lejos.nxt.*;
 import lejos.robotics.objectdetection.*;
 
 
-public class ObjectDetection
+public class ObjectDetection 
 {
     public static void main(String[] args)
     {
         System.out.println("Program 2");
         Button.waitForAnyPress();
-        
+       
         DifferentialPilot hook = new DifferentialPilot(56, 56, 162, Motor.B, Motor.C, false);
-        UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-        ObjectDetect listener = new ObjectDetect();
-        FeatureDetector fd = new RangeFeatureDetector(us, 50, 500);
-        fd.addListener(listener);
         hook.forward();
+        Button.waitForAnyPress();
+        hook.stop();
+        /*
+        
+        UltrasonicSensor us = new UltrasonicSensor(SensorPort.S3);
+        //ObjectDetection listener = new ObjectDetection();
+        FeatureDetector fd = new RangeFeatureDetector(us, 80, 500);
+        //fd.addListener(listener);
+        
         if (fd.scan() != null)
         {
-            hook.stop();
-        }
+            if (fd.scan().getRangeReading().getRange() < 5)
+            {
+                hook.stop();
+                System.out.println("object detected");
+            }
+        }  
         
-        
-        
-        
+        */
     }
+    
 }
